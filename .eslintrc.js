48ab7a44a0c944997534a09236ec04c14016c587
Paul Rock <paul@drteam.rocks>
2021-11-18 21:00:52 +0300

    Tune of static files

---
+++ .eslintrc.js
@@ -0,0 +1,38 @@
+module.exports = {
+  root: true, // Make sure eslint picks up the config at the root of the directory
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2021, // Use the latest ecmascript standard
+    sourceType: 'module', // Allows using import/export statements
+    ecmaFeatures: {
+      jsx: true, // Enable JSX since we're using React
+    },
+  },
+  settings: {
+    react: {
+      version: 'detect', // Automatically detect the react version
+    },
+  },
+  env: {
+    browser: true, // Enables browser globals like window and document
+    amd: true, // Enables require() and define() as global variables as per the amd spec.
+    node: true, // Enables Node.js global variables and Node.js scoping.
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:jsx-a11y/recommended',
+    'plugin:jest/recommended',
+    'plugin:prettier/recommended', // Make this the last element so prettier config overrides other formatting rules
+  ],
+  rules: {
+    'no-unused-vars': 0,
+    'jsx-a11y/no-autofocus': 'off',
+    'react/prop-types': 'off',
+    'prettier/prettier': [
+      'error',
+      { endOfLine: 'auto' },
+      { usePrettierrc: true },
+    ], // Use our .prettierrc file as source
+  },
+};
